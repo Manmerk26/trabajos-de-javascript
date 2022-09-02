9d09f2e8ca990a05cb1ef3b342b27f4b308e890f
juan mercado <juanmerk35@gmail.com>
2022-09-01 23:22:46 -0300

    arreglas java

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function calcularPrecio(entrada,cantidad){
+/* function calcularPrecio(entrada,cantidad){
     var precio = 0
 switch(entrada){
     case "babyshowers": 
@@ -38,3 +38,48 @@ let precio = calcularPrecio(entrada,cantidad);
 alert ("usted a comprado"+" "+entrada +" "+ "al precio de" + " " +precio);
 contacto = prompt("Ingrese su contacto y nos comunicaremos a la brevedad");
 alert ("Gracias por visitarnos");
+ */
+
+ class Productos{
+    constructor (producto,cantidad,precioConDesc,sinDesc){
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.precioConDesc = precioConDesc;
+        this.sinDesc = sinDesc;
+    }
+}
+const ListaProductos = [
+    {producto: "babyshowers" , cantidad: 20, precioConDesc: 900, sinDesc:50},
+    {producto: "cumpleaños" , cantidad: 2, precioConDesc: 8000, sinDesc:4500},
+    {producto: "comuniones" , cantidad: 10, precioConDesc: 1900, sinDesc:200},
+    {producto: "props" , cantidad: 20, precioConDesc: 1300, sinDesc:80},
+    {producto: "bautismos" , cantidad: 1, sinDesc: 5500},
+    {producto: "books" , cantidad: 1, sinDesc: 3200},
+    {producto: "gigantografias" , cantidad: 1, sinDesc: 7000},
+    {producto: "invitacionesDigitales" , cantidad: 1, sinDesc: 1300},
+];
+
+let producto = prompt("Ingrese el producto que desee");
+let cantidad = parseInt(prompt("Ingrese la cantida que desee"));
+if  (cantidad===1){
+
+}
+
+/* class Producto{
+    constructor (producto,cantidad,precioConDesc){
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.precioConDesc = precioConDesc;
+    }
+}
+
+  const productos =[];
+
+productos.push(new Producto("babyshowers" ,20 ,900));
+productos.push(new Producto("cumpleaños" ,2 ,8000));
+productos.push(new Producto("props" ,20 ,1300));
+productos.push(new Producto("bautismos" ,1 ,5500));
+productos.push(new Producto("books" , 1,3200));
+productos.push(new Producto("gigantografias" , 1,7000));
+productos.push (new Producto("invitacionesDigitales" ,1 ,1300)); */
+
