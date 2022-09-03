634046328d995fa8ecebaa1b3e67b30bff332a29
juan mercado <juanmerk35@gmail.com>
2022-09-02 22:54:36 -0300

    desafio complementario:arrays

--- index.js
+++ index.js
@@ -40,32 +40,13 @@ contacto = prompt("Ingrese su contacto y nos comunicaremos a la brevedad");
 alert ("Gracias por visitarnos");
  */
 
- class Productos{
+/* class Productos{
     constructor (producto,cantidad,precioConDesc,sinDesc){
         this.producto = producto;
         this.cantidad = cantidad;
         this.precioConDesc = precioConDesc;
         this.sinDesc = sinDesc;
-    }
-}
-const ListaProductos = [
-    {producto: "babyshowers" , cantidad: 20, precioConDesc: 900, sinDesc:50},
-    {producto: "cumpleaños" , cantidad: 2, precioConDesc: 8000, sinDesc:4500},
-    {producto: "comuniones" , cantidad: 10, precioConDesc: 1900, sinDesc:200},
-    {producto: "props" , cantidad: 20, precioConDesc: 1300, sinDesc:80},
-    {producto: "bautismos" , cantidad: 1, sinDesc: 5500},
-    {producto: "books" , cantidad: 1, sinDesc: 3200},
-    {producto: "gigantografias" , cantidad: 1, sinDesc: 7000},
-    {producto: "invitacionesDigitales" , cantidad: 1, sinDesc: 1300},
-];
-
-let producto = prompt("Ingrese el producto que desee");
-let cantidad = parseInt(prompt("Ingrese la cantida que desee"));
-if  (cantidad===1){
-
-}
-
-/* class Producto{
+    }*//* class Producto{
     constructor (producto,cantidad,precioConDesc){
         this.producto = producto;
         this.cantidad = cantidad;
@@ -82,4 +63,36 @@ productos.push(new Producto("bautismos" ,1 ,5500));
 productos.push(new Producto("books" , 1,3200));
 productos.push(new Producto("gigantografias" , 1,7000));
 productos.push (new Producto("invitacionesDigitales" ,1 ,1300)); */
+
+
+const ListaProductos = [
+    {producto: "babyshowers" , precioUnit:50, cantDesc:20, descuento:12},
+    {producto: "cumpleaños" , precioUnit:4500, cantDesc:2, descuento:13},
+    {producto: "comuniones" , precioUnit:200, cantDesc:10, descuento:10},
+    {producto: "props" , precioUnit:80 ,cantDesc:20, descuento:10},
+    {producto: "bautismos" , precioUnit:5500, cantDesc:2, descuento:13},
+    {producto: "books" , precioUnit:3200, cantDesc:2 ,descuento:10},
+    {producto: "gigantografias" , precioUnit:7000, cantDesc:2, descuento:18},
+    {producto: "invitacionesDigitales" , precioUnit:1300, cantDesc:5, descuento:15},
+];
+
+
+let nombre = prompt("Ingrese el producto que desee");
+let productoComprado = ListaProductos.find(item => item.producto === nombre);
+let cantidad = parseInt(prompt("Ingrese la cantidad"));
+let precio = productoComprado.precioUnit;
+if (cantidad > productoComprado.cantDesc) {
+    precio = precio - (precio/100 * productoComprado.descuento);
+};
+let aPagar= cantidad * precio;
+alert ("usted ha comprado"+" "+ nombre +" "+ "al precio de" + " " + aPagar);
+contacto = prompt("Ingrese su contacto y nos comunicaremos a la brevedad");
+if(contacto === " "){
+    alert(contacto);
+};
+alert ("Gracias por visitarnos");
+
+
+
+
 
