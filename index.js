c3b62006645b35905c6000531e5c21ec0b373aff
juan mercado <juanmerk35@gmail.com>
2022-09-03 23:21:40 -0300

    desafio complementario:arrays

--- index.js
+++ index.js
@@ -1,69 +1,3 @@
-/* function calcularPrecio(entrada,cantidad){
-    var precio = 0
-switch(entrada){
-    case "babyshowers": 
-    precio = 50 ;
-    break;
-    case "cumpleaños":
-    precio = 4500 ; 
-    break; 
-    case "comuniones":
-    precio = 200;
-    break;
-    case "props":
-    precio = 750;
-    break;  
-    case "bautismos":
-    precio = 5500 ; 
-    break;   
-    case "books":
-    precio = 3200;
-    break;
-    case "gigantografias":
-    precio = 7000;
-    break;
-    case "invitaciones digitales":
-    precio = 1300;
-    break;
-    default:
-        return Error
-    break;
-    
-}
-return precio * cantidad;
-}
-let entrada = prompt("Ingrese el producto");
-let cantidad = prompt("ingrese la cantidad");
-let precio = calcularPrecio(entrada,cantidad);
-alert ("usted a comprado"+" "+entrada +" "+ "al precio de" + " " +precio);
-contacto = prompt("Ingrese su contacto y nos comunicaremos a la brevedad");
-alert ("Gracias por visitarnos");
- */
-
-/* class Productos{
-    constructor (producto,cantidad,precioConDesc,sinDesc){
-        this.producto = producto;
-        this.cantidad = cantidad;
-        this.precioConDesc = precioConDesc;
-        this.sinDesc = sinDesc;
-    }*//* class Producto{
-    constructor (producto,cantidad,precioConDesc){
-        this.producto = producto;
-        this.cantidad = cantidad;
-        this.precioConDesc = precioConDesc;
-    }
-}
-
-  const productos =[];
-
-productos.push(new Producto("babyshowers" ,20 ,900));
-productos.push(new Producto("cumpleaños" ,2 ,8000));
-productos.push(new Producto("props" ,20 ,1300));
-productos.push(new Producto("bautismos" ,1 ,5500));
-productos.push(new Producto("books" , 1,3200));
-productos.push(new Producto("gigantografias" , 1,7000));
-productos.push (new Producto("invitacionesDigitales" ,1 ,1300)); */
-
 
 const ListaProductos = [
     {producto: "babyshowers" , precioUnit:50, cantDesc:20, descuento:12},
@@ -75,7 +9,7 @@ const ListaProductos = [
     {producto: "gigantografias" , precioUnit:7000, cantDesc:2, descuento:18},
     {producto: "invitacionesDigitales" , precioUnit:1300, cantDesc:5, descuento:15},
 ];
-
+/*const GranCompra = ListaProductos.filter()*/
 
 let nombre = prompt("Ingrese el producto que desee");
 let productoComprado = ListaProductos.find(item => item.producto === nombre);
@@ -87,9 +21,6 @@ if (cantidad > productoComprado.cantDesc) {
 let aPagar= cantidad * precio;
 alert ("usted ha comprado"+" "+ nombre +" "+ "al precio de" + " " + aPagar);
 contacto = prompt("Ingrese su contacto y nos comunicaremos a la brevedad");
-if(contacto === " "){
-    alert(contacto);
-};
 alert ("Gracias por visitarnos");
 
 
