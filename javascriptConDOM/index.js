0b31e089360ae0c1aa49a9fab1d379e0f34e0f64
juan mercado <juanmerk35@gmail.com>
2022-09-09 20:20:41 -0300

    interaccion con html

--- javascriptConDOM/index.js
+++ javascriptConDOM/index.js
@@ -1,13 +1,13 @@
 let contenedor = document.getElementById("contenedor");
 let ListaProductos = [
-    {producto: "babyshowers" , precioUnit:50, cantDesc:20, descuento:12},
-    {producto: "cumpleaños" , precioUnit:4500, cantDesc:2, descuento:13},
-    {producto: "comuniones" , precioUnit:200, cantDesc:10, descuento:10},
-    {producto: "props" , precioUnit:80 ,cantDesc:20, descuento:10},
-    {producto: "bautismos" , precioUnit:5500, cantDesc:2, descuento:13},
-    {producto: "books" , precioUnit:3200, cantDesc:2 ,descuento:10},
-    {producto: "gigantografias" , precioUnit:7000, cantDesc:2, descuento:18},
-    {producto: "invitacionesDigitales" , precioUnit:1300, cantDesc:5, descuento:15},
+    {producto: "babyshowers" , precioUnit:50, cantDesc:20, descuento:12,img: `http://creacionesajonjoli.000webhostapp.com/imagenes/tarjeta-baby-shower.webp`},
+    {producto: "cumpleaños" , precioUnit:4500, cantDesc:2, descuento:13,img: `http://creacionesajonjoli.000webhostapp.com/imagenes/felizcumple.jpeg`},
+    {producto: "comuniones" , precioUnit:200, cantDesc:10, descuento:10,img:`http://creacionesajonjoli.000webhostapp.com/imagenes/comunion.jpeg`},
+    {producto: "props" , precioUnit:80 ,cantDesc:20, descuento:10,img:`http://creacionesajonjoli.000webhostapp.com/imagenes/props.jpeg`},
+    {producto: "bautismos" , precioUnit:5500, cantDesc:2, descuento:13,img:`http://creacionesajonjoli.000webhostapp.com/imagenes/bautismo.jpeg`},
+    {producto: "books" , precioUnit:3200, cantDesc:2 ,descuento:10,img: `http://creacionesajonjoli.000webhostapp.com/imagenes/brisa.jpg`},
+    {producto: "gigantografias" , precioUnit:7000, cantDesc:2, descuento:18, img:`http://creacionesajonjoli.000webhostapp.com/imagenes/gigantografias.jpeg`},
+    {producto: "invitacionesDigitales" , precioUnit:1300, cantDesc:5, descuento:15,img:`http://creacionesajonjoli.000webhostapp.com/imagenes/invitacion-digital.jpg`},
 ];
     
 let todoslosNombres = ListaProductos.map((item) => item.producto);
@@ -19,11 +19,21 @@ while (nombre === ""){
    };
 let productoComprado = ListaProductos.find(item => item.producto === nombre);
 console.log(productoComprado);
+let cantidad = parseInt(prompt("Ingrese la cantidad,adquiriendo mas de 20 tiene un mayor descuento"));
+let precio = productoComprado.precioUnit;
+if (cantidad > productoComprado.cantDesc) {
+    precio = precio - (precio/100 * productoComprado.descuento);
+};
+let aPagar= cantidad * precio;
 for (let producto of productoComprado.producto) {
     let item = document.createElement("div");
-    item.innerHTML = `<h2>${productoComprado.producto}</h2>
-                      <p>PrecioUnit: $${productoComprado.precioUnit}</p>
-                      <b>Comprando mas de ${productoComprado.cantDesc} tendra un descuento del ${productoComprado.descuento}%</b>`;
+    item.innerHTML = `<div class="card">
+                      <img src=${productoComprado.img} class="imagen"></img>
+                      <h2>${productoComprado.producto}</h2>
+                      <p>PrecioUnit: $${productoComprado.precioUnit}</p>                      
+                      <b>Comprando mas de ${productoComprado.cantDesc} productos tendra un descuento del ${productoComprado.descuento}%</b>
+                      <p>Precio Total: $${aPagar}</p>
+                      </div>`;
                       console.log(item);
     contenedor.append(item);
 break;
