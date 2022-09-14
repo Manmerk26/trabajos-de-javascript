b737c305d79f8afa0b7d4130738c870e405f2bcb
juan mercado <juanmerk35@gmail.com>
2022-09-13 23:15:46 -0300

    eventos

--- javascriptConDOM/index.js
+++ javascriptConDOM/index.js
@@ -1,6 +1,32 @@
-import {ListaProductos} from "./data.js";
+let ListaProductos = [
+    {id:1,producto: "babyshowers" , precioUnit:50, cantDesc:20, descuento:12,img: `http://creacionesajonjoli.000webhostapp.com/imagenes/tarjeta-baby-shower.webp`},
+    {id:2,producto: "cumpleaños" , precioUnit:4500, cantDesc:2, descuento:13,img: `http://creacionesajonjoli.000webhostapp.com/imagenes/felizcumple.jpeg`},
+    {id:3,producto: "comuniones" , precioUnit:200, cantDesc:10, descuento:10,img:`http://creacionesajonjoli.000webhostapp.com/imagenes/comunion.jpeg`},
+    {id:4,producto: "props" , precioUnit:80 ,cantDesc:20, descuento:10,img:`http://creacionesajonjoli.000webhostapp.com/imagenes/props.jpeg`},
+    {id:5,producto: "bautismos" , precioUnit:5500, cantDesc:2, descuento:13,img:`http://creacionesajonjoli.000webhostapp.com/imagenes/bautismo.jpeg`},
+    {id:6,producto: "books" , precioUnit:3200, cantDesc:2 ,descuento:10,img: `http://creacionesajonjoli.000webhostapp.com/imagenes/brisa.jpg`},
+    {id:7,producto: "gigantografias" , precioUnit:7000, cantDesc:2, descuento:18, img:`http://creacionesajonjoli.000webhostapp.com/imagenes/gigantografias.jpeg`},
+    {id:8,producto: "invitacionesDigitales" , precioUnit:1300, cantDesc:5, descuento:15,img:`http://creacionesajonjoli.000webhostapp.com/imagenes/invitacion-digital.jpg`},
+];
+
 let contenedor = document.getElementById("contenedor");
-   
+const fragment = document.createDocumentFragment();
+const template = document.querySelector("#template-li").content;
+
+
+ListaProductos.forEach((item) => {
+    let item = document.createElement("div");
+    item.innerHTML = `<div class="card">
+                      <img src=${productoComprado.img} class="imagen"></img>
+                      <h2>${productoComprado.producto}</h2>
+                      <p>PrecioUnit: $${productoComprado.precioUnit}</p>                      
+                      <b>Comprando mas de ${productoComprado.cantDesc} productos tendra un descuento del ${productoComprado.descuento}%</b>
+                      <p>Precio Total: $${aPagar}</p>
+                      </div>`;
+                      console.log(item);
+    contenedor.append(item);
+break;
+}
 
 
 
@@ -35,8 +61,9 @@ let contenedor = document.getElementById("contenedor");
 
 
 
-/* 
-let todoslosNombres = ListaProductos.map((item) => item.producto);
+
+ 
+/* let todoslosNombres = ListaProductos.map((item) => item.producto);
 listaNombres=todoslosNombres.join( " - ");
 let nombre="";
 while (nombre === ""){
@@ -64,4 +91,4 @@ for (let producto of arrProdComp) {
                       console.log(item);
     contenedor.append(item);
 break;
-} */
+*
