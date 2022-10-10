ea07b61dd75c69ce32ee026eea87a4d06cdf945f
juan mercado <juanmerk35@gmail.com>
2022-10-10 20:59:26 -0300

    boton terminar compra

--- javascriptEventos/index2.js
+++ javascriptEventos/index2.js
@@ -148,6 +148,80 @@ function dibujarCarrito() {
                 dibujarCarrito();
             
         });
+
+          //Terminar compra
+          let botonTerminarCompra = document.getElementsByClassName(`btn-primary-${contenedorCarritoCompras}`);
+
+          botonTerminarCompra.addEventListener('onclick', (e) => {
+             //Enviamos informacion en forma de parametros a traves de la url
+        let params = {
+            user_id: 'user_yVrRAHjsxIJshwpBg',
+            service_id: 'default_service',
+            template_id: 'template_lnzcymw',
+            template_params: {
+            nombre: elemento.producto.nombre,
+            cantidad: elemento.cantidad,
+            total: totalCompra, 
+            }
+        };
+    
+        let headers = {
+            'Content-type': 'application/json'/*Tenemos la propiedad Content-type, con el valor que nos indica
+            la documentacion de la API*/
+        };
+    
+        //Definimos el segundo parametro opcional del fetch de tipo object en el cual transferimos datos en formato JSON
+        let options = {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(params)
+        };
+    
+        fetch('https://api.emailjs.com/api/v1.0/email/send', options)
+        .then(async (httpResponse) => {//Con async-await esperamos a que se resuelva la promesa para continuar con la siguiente instruccion
+            if (httpResponse.ok) {
+                
+
+                setTimeout(() => {
+                    enviarSolicitud.value = 'Enviar';
+                    Swal.fire({
+                        imageUrl: '../img/logo.png',
+                        imageHeight: 80,
+                        imageWidth: 80,
+                        background: '#be1e24',
+                        width: 300,
+                        title: '<h5 class="text-warning">Formulario enviado</h5>',
+                        showConfirmButton: true,
+                    }).then(function () {
+                        window.location.href = "../index.html";
+                    })
+
+                }, 2000);
+
+
+            } else {
+                const text = await httpResponse.text();
+                return await Promise.reject(text);
+            }
+        })
+        .catch((error) => {
+            enviarSolicitud.value = 'Enviar';
+            console.log(error)
+            Swal.fire({
+                imageUrl: '../img/logo.png',
+                imageHeight: 80,
+                imageWidth: 80,
+                background: '#be1e24',
+                width: 300,
+                title: `<h5 class="text-warning">No se pudo realizar el envio del formulario. Intente nuevamente.</h5>`,
+                showConfirmButton: true,
+              })
+        });
+        
+        
+
+          } )
+
         
     });
 
